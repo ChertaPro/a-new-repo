7088605ee8252eae9f234b7a3bef5965509a4aec
ChertaPro <chertapro@gmail.com>
2026-07-16 15:13:44 -0400

    Update(consts): site title and description for new project focus

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'A New Repo';
+export const SITE_DESCRIPTION = 'Proyectos, herramientas y la vida de un estudiante de Ciencias de la Computación. Que la Fuerza (del código) te acompañe.';
